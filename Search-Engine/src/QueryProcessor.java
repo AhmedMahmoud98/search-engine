5739cbbaafd79ebf963251696775940846b28782
Alii-Khaled <id.ali1230@gmail.com>
2020-05-08 13:39:15 +0200

    Added Query Processor

---
+++ Search-Engine/src/QueryProcessor.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class QueryProcessor {
+    private static String query;
+    private QueryProcessor() { query = ""; }
+    public static void setQuery(String q){
+        query = q;
+    }
+    public static String getQuery(){
+        return query;
+    }
+    private static char[] stringToChar(String str) {
+        char[] ch = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            ch[i] = str.charAt(i);
+        }
+        return ch;
+    }
+    private static ArrayList<String> process(){
+        query = query.toLowerCase();
+        // query = query.replaceAll("[^a-zA-Z0-9 ]", "");
+        String[] temp = query.split("\\s+");
+        List<String> stopWords = StopWords.getStopWords();
+
+        ArrayList<String> queryProcessed = new ArrayList<String>();
+        Stemmer stemmer = new Stemmer();
+        boolean phrase = false;
+
+        for (String s : temp) {
+            if (s.startsWith("\"")){
+                phrase = true;
+            }
+            if (phrase) {
+                if (s.endsWith("\"")){
+                    phrase = false;
+                }
+                queryProcessed.add(s.replaceAll("[^a-zA-Z0-9 ]", ""));
+            }
+            else if (!stopWords.contains(s)) {
+                s = s.replaceAll("[^a-zA-Z0-9 ]", "");
+                stemmer.add(stringToChar(s), s.length());
+                stemmer.stem();
+                queryProcessed.add(stemmer.toString());
+                // stemmer.reset();
+            }
+        }
+        System.out.println("Processed String Array: " + queryProcessed);
+        return queryProcessed;
+    }
+    public static void main(String[] Args)
+    {
+        String query = "Hello man Swapping \"My Name IS Body\" my \"games is not easier.\" man.";
+        QueryProcessor.setQuery(query);
+        ArrayList<String> processed = QueryProcessor.process();
+    }
+}
