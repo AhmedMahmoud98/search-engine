239376219a75b584ecca8343aa440c604de6fac4
phella <philopateernabil2021@gmail.com>
2020-05-14 21:34:28 +0200

    ehance save documents terms query performance

--- Search-Engine/src/DB/DbManager.java
+++ Search-Engine/src/DB/DbManager.java
@@ -34,18 +34,12 @@ public class DbManager {
         DBCollection collection = database.getCollection("Term");
 
         for (Map.Entry<String,Set<Integer>> entry : terms.entrySet()) {
-            //System.out.println(entry.getValue());
-//            DBObject term = new BasicDBObject()
-//                    .append("term", entry.getKey())
-//                    .append("termFrequency", entry.getValue().size())
-//                    .append("documents", entry.getValue());
 
             collection.update(new BasicDBObject("term", entry.getKey()),
                     new BasicDBObject("$inc", new BasicDBObject("termFrequency", entry.getValue().size()))
                                       .append("$push" , new BasicDBObject("documents", new BasicDBObject("$each", entry.getValue())))
                                        , true
                                        , false);
-            //collection.insert(term);
         }
     }
     public void saveRobot( Map<String , ArrayList<String>> robots){
@@ -60,10 +54,10 @@ public class DbManager {
 
             collection.update(new BasicDBObject("Link", entry.getKey()),
                     new BasicDBObject
-                                      ("$push" , new BasicDBObject("Disallowed", new BasicDBObject("$each", entry.getValue())))
-                                       , true
-                                       , false);
-           
+                            ("$push", new BasicDBObject("Disallowed", new BasicDBObject("$each", entry.getValue())))
+                    , true
+                    , false);
+
             //collection.insert(term);
         }
     }
@@ -94,6 +88,7 @@ public class DbManager {
     public void saveDocumentCollection( Map<termDocumentKey, List<Integer>> terms){
         DBCollection collection = database.getCollection("Document");
 
+        List<DBObject> entreis= new ArrayList<DBObject>();
         for (Map.Entry<termDocumentKey, List<Integer>>  termDocument : terms.entrySet()) {
             DBObject entry = new BasicDBObject()
                     .append("term", termDocument.getKey().term)
@@ -101,17 +96,15 @@ public class DbManager {
                     .append("termDocumentFrequency", termDocument.getValue().size())
                     .append("positions" , termDocument.getValue())    ;
 
-            //System.out.println(entry);
-
+            entreis.add(entry);
+            /*
             collection.update(new BasicDBObject("term", termDocument.getKey().term)
                                                 .append("document",termDocument.getKey().docID)
                     , entry
                     , true
                     , false);
-
-
-             //collection.insert(entery);
-
+            */
         }
+        collection.insert(entreis);
     }
 }
