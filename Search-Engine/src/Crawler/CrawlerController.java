b29198eb4b0925b3d2d72d6e1410791139dfa538
AlyRamzy <alyhassan62@yahoo.com>
2020-05-18 01:42:46 +0200

    Recrawl

--- Search-Engine/src/Crawler/CrawlerController.java
+++ Search-Engine/src/Crawler/CrawlerController.java
@@ -1,8 +1,11 @@
 package Crawler;
-import org.jsoup.Jsoup;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 import com.mongodb.DBCollection;
@@ -12,17 +15,19 @@ import com.mongodb.util.JSON;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 import DB.*;
-
 
 // JAR File is added to ClassPath in Libraries and marked true on order and export 
 
@@ -32,24 +37,32 @@ public class CrawlerController {
 	public static Map<String, ArrayList<String>> ROBOTS_DISALLOWED;
 	public static int NUMBER_OF_WEBSITES;
 	public static ArrayList<CrawlerObject> LINKS;
+	public static ArrayList<SeedsObject> SEEDS;
+	public static  ArrayList<String> INITIAL_SEEDS;
 
 	public static void main(String[] args) throws InterruptedException {
 		// Creating The Links Container and The Websites That is Disallowed To join
-		ArrayList<String> seeds = new ArrayList<String>();
+		SEEDS = new ArrayList<SeedsObject>();
 		ROBOTS_DISALLOWED = new HashMap<String, ArrayList<String>>();
 		LINKS = new ArrayList<CrawlerObject>();
-		seeds.add("https://www.geeksforgeeks.org/greedy-algorithms");
-		seeds.add("https://www.geeksforgeeks.org/computer-network-tutorials");
+		INITIAL_SEEDS = new ArrayList<String>();
+		//////////////////////////////////////Initial Seeds for Crawling///////////////////////////////////////////////////////////////////
+		INITIAL_SEEDS.add("https://www.geeksforgeeks.org/greedy-algorithms");
+		INITIAL_SEEDS.add("https://www.geeksforgeeks.org/computer-network-tutorials");
 
-		//Loading Previous State Of The Crawler From DataBase 
+		// Loading Previous State Of The Crawler From DataBase
 		GetCrawledLinks();
 		GetRobots();
+		GetSeeds();
+		if(SEEDS.size()==0) {
+			LoadFirstSeeds();
+		}
 		if (LINKS.size() == 0) {
-			for (int i = 0; i < seeds.size(); i++) {
-				if (Crawler.CheckRobots(seeds.get(i))) {
+			for (int i = 0; i < SEEDS.size(); i++) {
+				if (Crawler.CheckRobots(SEEDS.get(i).getLink())) {
 					CrawlerObject c = new CrawlerObject();
 					c.setNumberOfURLs(0);
-					c.setLinkURL(seeds.get(i));
+					c.setLinkURL(SEEDS.get(i).getLink());
 					LINKS.add(c);
 				}
 			}
@@ -64,7 +77,8 @@ public class CrawlerController {
 		if (currentIndex < 0) {
 			currentIndex = 0;
 		}
-		//Crawling New Pages If new Pages are needed more than database or if the database is empty 
+		// Crawling New Pages If new Pages are needed more than database or if the
+		// database is empty
 
 		// Starting The Threads To Start Crawling
 		int numberOfThreads = 5;
@@ -86,29 +100,101 @@ public class CrawlerController {
 
 				}
 
-				}
-			
+			}
+
 			for (int i = 0; i < threads.size(); i++) {
 				threads.get(i).join();
-				
+
 			}
 			if (threads.size() == 0) {
 				break;
 			}
-	
+		}
 
+			// Save your Output To DataBase
+			SaveRobots();
+			SaveLinks();
+			// ReCrawlling and ReCrawlling Conditions and Time To Check ReCrawlling
+			boolean recrawl = false;
+			//ReCrawling Condition if any document of the seeds Changed
+			for(int i =0;i<SEEDS.size();i++) {
+				Document document = null;
+				try {
+					document = Jsoup.connect(SEEDS.get(i).getLink()).get();
+					if(!document.body().text().equals(SEEDS.get(i).getBody())) {
+						recrawl = true;
+						
+					}
+					
+				} catch (IOException e) {
+					
+				}
+			}
+			//ReCrawling
+			if(recrawl) {
+				System.out.println("Needs To Recrawl");
+				LINKS.clear();//Clear Old Crawled and start Recrawling again
+				currentIndex = 0;
+				for (int i = 0; i < SEEDS.size(); i++) {
+					if (Crawler.CheckRobots(SEEDS.get(i).getLink())) {
+						CrawlerObject c = new CrawlerObject();
+						c.setNumberOfURLs(0);
+						c.setLinkURL(SEEDS.get(i).getLink());
+						LINKS.add(c);
+					}
+				}
+				System.out.println(LINKS.size());
+				while (LINKS.size() < NUMBER_OF_WEBSITES) {
+					threads.clear();
+					for (int j = 0; j < numberOfThreads; j++) {
+						if (currentIndex < LINKS.size()) {
+							CrawlerObject current = LINKS.get(currentIndex);
+							Crawler c = new Crawler(current, LINKS, j);
+							current.setVisited(true);
+							Thread t = new Thread(c);
+							threads.add(t);
+							t.start();
+							currentIndex++;
+
+						}
+
+					}
+
+					for (int i = 0; i < threads.size(); i++) {
+						System.out.println(LINKS.size());
+						threads.get(i).join();
+
+					}
+					if (threads.size() == 0) {
+						break;
+					}
+				}
+				SaveRobots();
+				SaveLinks();
+				LoadFirstSeeds();
+			}
+			else {
+				System.out.println("Doesnt Need To Recrawl");
+				
+			}
+			
 		
-		//Save your Output To DataBase
-		SaveRobots();
-		SaveLinks();
-		//ReCrawlling  and  ReCrawlling Conditions and Time To Check ReCrawlling
 	}
+	/////////////////////////////////////////////////Create Initial Seeds To The DataBase///////////////////////////////////////////////////////
+	public static void LoadFirstSeeds(){
+		for(int i =0;i<INITIAL_SEEDS.size();i++) {
+			Document document = null;
+			try {
+				document = Jsoup.connect(INITIAL_SEEDS.get(i)).get();
+				SEEDS.add(new SeedsObject(INITIAL_SEEDS.get(i),document.body().text()));
+			} catch (IOException e) {
+				
+			}
+		}
+		SaveSeeds();
+		
 	}
-		
-
-		
-
-
+	/////////////////////////////////////////////////SAVE Items To The DataBase ////////////////////////////////////////////////////////////////
 
 	public static void SaveRobots() {
 		DbManager DBManager = DbManager.getInstance();
@@ -119,6 +205,12 @@ public class CrawlerController {
 		DbManager DBManager = DbManager.getInstance();
 		DBManager.saveCrawler(LINKS);
 	}
+	
+	public static void SaveSeeds() {
+		DbManager DBManager = DbManager.getInstance();
+		DBManager.saveSeeds(SEEDS);
+	}
+	/////////////////////////////////////////////Load Items From DataBase/////////////////////////////////////////////////////////////////////////
 
 	public static void GetRobots() {
 		DbManager DBManger = DbManager.getInstance();
@@ -153,12 +245,20 @@ public class CrawlerController {
 
 		}
 	}
+	public static void GetSeeds() {
+		DbManager DBManger = DbManager.getInstance();
+		DBCollection SeedsDB = DBManger.getSeeds().getCollection();
+		Iterator<DBObject> objects = SeedsDB.find().iterator();
+
+		while (objects.hasNext()) {
+			Map SeedFromDB = objects.next().toMap();
+			String linkName = (String) SeedFromDB.get("Link");
+			String content  = (String) SeedFromDB.get("Content");
+
+			SEEDS.add(new SeedsObject(linkName,content));
+		}
+	}
 }
-
-
-
-
-
 
 class Crawler implements Runnable {
 	private ArrayList<CrawlerObject> links;
@@ -172,7 +272,9 @@ class Crawler implements Runnable {
 		this.ThreadNumber = ThreadNumber;
 
 	}
-	//////////////////////////////////////////////////////Add Website That Cites another Website /////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////// Add Website That Cites
+	////////////////////////////////////////////////////// another Website
+	////////////////////////////////////////////////////// /////////////////////////////////////////////////////////
 
 	public void AddRefer(String elementURL) {
 		for (int i = 0; i < links.size(); i++) {
@@ -184,9 +286,18 @@ class Crawler implements Runnable {
 
 		}
 	}
+	//////////////////////////////////////////To Get Domain Name From Given URL ////////////////////////////////////////////////////////////
+	public static String getDomainName(String url) throws URISyntaxException {
+	    URI uri = new URI(url);
+	    String domain = uri.getHost();
+	   // return domain.startsWith("www.") ? domain.substring(4) : domain;
+	    return domain;
+	}
 
-	/////////////////////////////////////////////////// Check if the url already exists so not to crawl it again///////////////////////////////////
-	
+	/////////////////////////////////////////////////// Check if the url already
+	/////////////////////////////////////////////////// exists so not to crawl it
+	/////////////////////////////////////////////////// again///////////////////////////////////
+
 	public boolean CheckExist(String URL) {
 		for (int i = 0; i < links.size(); i++) {
 			if (links.get(i).getLinkURL().equals(URL)) {
@@ -195,17 +306,21 @@ class Crawler implements Runnable {
 		}
 		return false;
 	}
-	/////////////////////////////////////////////////////////Crawl Current URL///////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////// Crawl Current
+	///////////////////////////////////////////////////////// URL///////////////////////////////////////////////////////////////////////
 
 	private void getPageLinks(String URL) {
-		
+
 		Document document;
 		try {
 			document = Jsoup.connect(URL).get();
 			
+			
+			
+			
+
 			Elements linksOnPage = document.select("a[href]");
 			mCrawlerObj.setNumberOfURLs(linksOnPage.size());
-
 			
 
 			for (Element page : linksOnPage) {
@@ -217,8 +332,6 @@ class Crawler implements Runnable {
 						CrawlerObject toBeAdded = new CrawlerObject();
 						toBeAdded.setLinkURL(page.attr("abs:href"));
 						links.add(toBeAdded);
-
-						
 
 					}
 				}
@@ -232,41 +345,53 @@ class Crawler implements Runnable {
 		// }
 
 	}
-	
-	
-	//////////////////////////////////////////Check If Its Allowed To Crawl This Link or Not From The Robot File //////////////////////////////////////
+
+	////////////////////////////////////////// Check If Its Allowed To Crawl This
+	////////////////////////////////////////// Link or Not From The Robot File
+	////////////////////////////////////////// //////////////////////////////////////
 
 	public static boolean CheckRobots(String URL) {
 		////////////////////////////////// Filter The URL to its basic website
 		////////////////////////////////// ////////////////////////////////////
-		String[] temp = URL.split("/");
+		/*
+		 * String[] temp = URL.split("/"); String filteredURL = null; if
+		 * ((temp[0].equals("https:") || temp[0].equals("http:"))) { String[] secondTemp
+		 * = temp[2].split("\\.");
+		 * 
+		 * if (secondTemp[0].equals("www")) { filteredURL = temp[0] + "//" + temp[2] +
+		 * "/robots.txt";
+		 * 
+		 * } else { filteredURL = "https://www." + secondTemp[1] + ".com/robots.txt";
+		 * 
+		 * }
+		 * 
+		 * } else { filteredURL = temp[0] + "/robots.txt";
+		 * 
+		 * }
+		 */
 		String filteredURL = null;
-		if ((temp[0].equals("https:") || temp[0].equals("http:"))) {
-			String[] secondTemp = temp[2].split("\\.");
-
-			if (secondTemp[0].equals("www")) {
-				filteredURL = temp[0] + "//" + temp[2] + "/robots.txt";
-
-			} else {
-				filteredURL = "https://www." + secondTemp[1] + ".com/robots.txt";
-
-			}
-
-		} else {
-			filteredURL = temp[0] + "/robots.txt";
-
+		try {
+			filteredURL = "https://"+getDomainName(URL)+"/robots.txt";
+		} catch (URISyntaxException e2) {
+			
 		}
 		////////////////////////////////////// NEW Robot File Need To be Fetched
 		////////////////////////////////////// //////////////////////////////////
 
 		if (!CrawlerController.ROBOTS_DISALLOWED.containsKey(filteredURL)) {
 			ArrayList<String> disallowed = new ArrayList<String>();
+			
 
 			try {
+				
 				URL url = new URL(filteredURL);
 
+				
 				URLConnection urlcon = url.openConnection();
 				InputStream stream = urlcon.getInputStream();
+			
+					
+				
 				Scanner sc = new Scanner(stream);
 				boolean foundUser = false;
 
