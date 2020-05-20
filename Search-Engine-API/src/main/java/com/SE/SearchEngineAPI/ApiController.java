66d7962ca5a929221a987bc9a1a716089ce23728
phella <philopateernabil2021@gmail.com>
2020-05-20 20:08:39 +0200

    add sugestion end point

--- Search-Engine-API/src/main/java/com/SE/SearchEngineAPI/ApiController.java
+++ Search-Engine-API/src/main/java/com/SE/SearchEngineAPI/ApiController.java
@@ -3,19 +3,23 @@ import java.util.*;
 
 import Models.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
   @Autowired
   VisitedUrlsRepository visitedUrlsRepository;
-
+  @Autowired
+  SuggestionsRepository suggestionsRepository;
+  
   @GetMapping("/images")
   public ResponseEntity<List<Image>> getImages(@RequestBody Query query ) {
 	  try {
@@ -44,10 +48,12 @@ public class ApiController {
 		     *  then return array of Pages 
 		     */
 		    
+		    
+		    suggestionsRepository.incFreq(query.getQueryString());
 		    if (Pages.isEmpty()) {
 		      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		    }
-
+		    
 		    return new ResponseEntity<>(Pages, HttpStatus.OK);
 		  } catch (Exception e) {
 		    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -71,6 +77,27 @@ public class ApiController {
 		  } catch (Exception e) {
 		    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		  }
+  }
+  
+  @GetMapping("/suggestions")
+  public ResponseEntity<List<String>> getSuggestions (@RequestParam String query) {
+	  
+	  try {
+		    List<Suggestion> suggestions = new ArrayList<>();
+		    
+		    System.out.println(query);
+		    suggestions = suggestionsRepository.findBySearchStringStartingWithOrderByFrequencyDesc(query.toLowerCase() , PageRequest.of(0, 7) );
+		    System.out.println(suggestions);
+		    
+		    if (suggestions.isEmpty()) {
+		      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		    }
+
+		    return new ResponseEntity<>(Suggestion.searchStrings(suggestions), HttpStatus.OK);
+		  } catch (Exception e) {
+		    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		  }
+		  
   }
 
   @PostMapping("/VisitedUrls")
---
+++ Search-Engine-API/src/main/java/com/SE/SearchEngineAPI/SuggestionsRepository.java
@@ -0,0 +1,25 @@
+package com.SE.SearchEngineAPI;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import Models.Suggestion;
+public interface SuggestionsRepository extends
+		MongoRepository<Suggestion, String> {
+	
+	
+	@Query("{'searchString': ?0 } , {'$inc' : {'frequency':1}} ")
+	public void incFreq(String query);
+	
+	/*
+	@Query("{ 'searchString':{$regex:?0*} , $sort : { frequency : -1 } , $limit : 7 } , { searchString: 1}")
+	public List<String> matchQuery(String query);
+	*/
+	
+	public List<Suggestion> findBySearchStringStartingWithOrderByFrequencyDesc(String queryBegin , PageRequest pageRequest);
+	
+}
