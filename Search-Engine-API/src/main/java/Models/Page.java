0991047c12dcdbbacd4ab15c2d101ac5dee50442
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2020-05-27 22:45:21 +0200

    Parse the url that will be sent to the frontend

--- Search-Engine-API/src/main/java/Models/Page.java
+++ Search-Engine-API/src/main/java/Models/Page.java
@@ -3,6 +3,10 @@ package Models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+
 @Document(collection = "Pages")
 public class Page {
   @Id
@@ -13,11 +17,42 @@ public class Page {
   public Page() {
 
   }
+  
+  public Page(String _url, String queryTerm1, String queryTerm2, int Dummy) {
+	  this.url = _url;
+	try {
+		org.jsoup.nodes.Document doc = Jsoup.connect(this.url).get();
+		this.title = doc.getElementsByTag("title").text();
+		int firstParagraphFirstIndex, firstParagraphLastIndex, secondParagraphFirstIndex, secondParagraphLastIndex;
+		String docText = doc.text();
+		firstParagraphFirstIndex = docText.indexOf(queryTerm1);
+		firstParagraphLastIndex = docText.indexOf(" ", firstParagraphFirstIndex + 100);
+		this.summary = docText.substring(firstParagraphFirstIndex, firstParagraphLastIndex) + "... ";
+		
+		if(queryTerm2 != "") {
+			secondParagraphFirstIndex = docText.indexOf(queryTerm2);
+			secondParagraphLastIndex = docText.indexOf(" ", secondParagraphFirstIndex + 100);
+		}
+		else {
+			secondParagraphFirstIndex = docText.indexOf(queryTerm1, firstParagraphLastIndex + 1);
+			secondParagraphLastIndex = docText.indexOf(" ", secondParagraphFirstIndex + 100);
+		}
+		
+		this.summary += docText.substring(secondParagraphFirstIndex, secondParagraphLastIndex) + ".";
+		
+		/* Make The Query String bold */
+		this.summary = this.summary.replace(queryTerm1, "<b>"+ queryTerm1+"</b>").replace(queryTerm2, "<b>"+ queryTerm2+"</b>");;
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	  
 
-  public Page(String tit, String ur, String summry) {
-    this.title = tit;
-    this.url = ur;
-    this.summary = summry;
+  }
+
+  public Page(String _title, String _url, String _summary) {
+    this.title = _title;
+    this.url = _url;
+    this.summary = _summary;
   }
 
   public String getTitle() {
